71ad8a8c31b48dba55329f64d68ae4194c6fa567
Emmanuel Estrella <estrellaemmanuel74@gmail.com>
2022-10-25 14:12:35 -0500

    Comments: Fix typo

--- board.rb
+++ board.rb
@@ -4,7 +4,7 @@ module Board
     empty_line = "         |         |         "
     divider = "_________|_________|_________"
     # 14 lines is the height of the board
-    # Lines 4 and 9 and the horizontal dividers
+    # Lines 4 and 9 are the horizontal dividers
     14.times { |i| puts i == 4 || i == 9 ? divider : empty_line}
     puts ''
   end
