92c96bbe6f4ab51183bb1b44ad57877aecb77c5b
Emmanuel Estrella <estrellaemmanuel74@gmail.com>
2022-10-25 14:11:39 -0500

    Add method to generate an empty board

---
+++ board.rb
@@ -0,0 +1,11 @@
+module Board
+  def generate_empty_board()
+    puts ''
+    empty_line = "         |         |         "
+    divider = "_________|_________|_________"
+    # 14 lines is the height of the board
+    # Lines 4 and 9 and the horizontal dividers
+    14.times { |i| puts i == 4 || i == 9 ? divider : empty_line}
+    puts ''
+  end
+end
