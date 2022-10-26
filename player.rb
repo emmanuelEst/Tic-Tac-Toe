10da43f6979d1a260e70cc13f8d8489c03ba4ffd
Emmanuel Estrella <estrellaemmanuel74@gmail.com>
2022-10-25 22:21:26 -0500

    Code: Create player class preliminary code

---
+++ player.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: false
+
+# Player class used to contain information like:
+#   name, team, and current plays
+class Player
+  attr_reader name:, team:
+  attr_accessor plays:
+  
+  def initialize(name, team)
+    @name = name
+    @team = team
+    @plays = {}
+  end
+end
