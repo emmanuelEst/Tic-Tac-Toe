78b60012dc0f643e869df7b042e6b63192e57bac
Emmanuel Estrella <estrellaemmanuel74@gmail.com>
2022-10-25 22:42:23 -0500

    Code: Fix accessor typos

--- player.rb
+++ player.rb
@@ -3,9 +3,9 @@
 # Player class used to contain information like:
 #   name, team, and current plays
 class Player
-  attr_reader name:, team:
-  attr_accessor plays:
-  
+  attr_reader :name, :team
+  attr_accessor :plays
+
   def initialize(name, team)
     @name = name
     @team = team
