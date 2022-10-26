50398ff975016cd4faf1d8f943c5dfdff2255738
Emmanuel Estrella <estrellaemmanuel74@gmail.com>
2022-10-25 22:13:05 -0500

    Convention: Refactor code to follow rubocop convention

--- board.rb
+++ board.rb
@@ -1,11 +1,14 @@
+# frozen_string_literal: false
+
+# Used to display the different states of the board
 module Board
-  def generate_empty_board()
+  def generate_empty_board
     puts ''
-    empty_line = "         |         |         "
-    divider = "_________|_________|_________"
+    empty_line = '         |         |         '
+    divider = '_________|_________|_________'
     # 14 lines is the height of the board
     # Lines 4 and 9 are the horizontal dividers
-    14.times { |i| puts i == 4 || i == 9 ? divider : empty_line}
+    14.times { |i| puts [4, 9].include?(i) ? divider : empty_line }
     puts ''
   end
 end
